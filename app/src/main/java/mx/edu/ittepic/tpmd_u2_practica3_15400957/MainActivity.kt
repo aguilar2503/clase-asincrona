ba4ab1cfe37d849795efc1b4801d479d7046c2fb
aguilar2503 <aguilar_.roberto@hotmail.com>
2019-10-09 13:12:07 -0600

    metodo para guardar los num

--- app/src/main/java/mx/edu/ittepic/tpmd_u2_practica3_15400957/MainActivity.kt
+++ app/src/main/java/mx/edu/ittepic/tpmd_u2_practica3_15400957/MainActivity.kt
@@ -1,5 +1,9 @@
 package mx.edu.ittepic.tpmd_u2_practica3_15400957
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.widget.Toast
+import java.io.OutputStreamWriter
 import android.content.Context
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
@@ -32,13 +36,14 @@ class MainActivity : AppCompatActivity() {
     class Task1(num1:String ,num2:String,context: Context):AsyncTask<Void,Void, List<Int>>(){ //doInBackground
         var num1 = num1.toInt()
         var num2 = num2.toInt()
+        var context = context
 
         override fun onPostExecute(result: List<Int>?) {
             super.onPostExecute(result)
             var cont =0;
             var et = "";
             var numerosPrimos="Numero primos: "
-            (0..1999).forEach({
+            (0..1999).forEach{
                 cont =0
                 et =result?.get(it).toString()
                 (1..et.toInt()).forEach {
@@ -46,12 +51,28 @@ class MainActivity : AppCompatActivity() {
                         cont++
                     }
                 }
+                if (cont <= 2 && et.toInt()>1) {
+                    numerosPrimos=numerosPrimos+et+", "
+                } else {
 
-            })
+                }
+            }
+                    println(numerosPrimos)
+                    guardar(numerosPrimos)
+
+        }
+
+        fun guardar(numerosPrimos:String){
+            val guardarArchivo = OutputStreamWriter(context.openFileOutput("primos.txt", Activity.MODE_PRIVATE)) //construye un archivo en memoria interna o externa, output para guardar
+            guardarArchivo.write(numerosPrimos)
+            guardarArchivo.flush()
+            guardarArchivo.close()
+            println("GUARDADO")
+            Toast.makeText(context,"¡Números generados y guardados correctamente!",Toast.LENGTH_LONG).show()
         }
 
         override fun doInBackground(vararg p0: Void?): List<Int> {
-          val task = List (200){ Random.nextInt(num1,num2) }
+          val task = List (2000){ Random.nextInt(num1,num2) }
             println(task)
 
             return task
